21a0c59b7c44b9dd5d227e685d221a6a1fd2227c
2883857@student.edu.az <2883857@student.edu.az>
2023-01-06 20:57:13 +0400

    modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,9 @@ $(document).ready(function () {
 });
 
 let burger = document.querySelector(".burger");
-console.log("ggg");
 let hamburgerDrop = document.getElementById("hamburgerDrop");
 let xburger = document.getElementById("x-burger");
 
-console.log("vvv");
 
 burger.addEventListener("click", function () {
 
@@ -20,6 +18,28 @@ xburger.addEventListener("click", function () {
 
     hamburgerDrop.style.transform = "TranslateX(-100rem)"
     hamburgerDrop.style.transition = "2s";
+
+})
+
+function increaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+
+function decreaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+}
+
+let cardHeader = document.querySelector(".card-header")
+console.log(cardHeader);
+cardHeader.addEventListener("click", function () {
+    let chevron=document.querySelector(".i").classList.toggle("iactive")
 
 })
 
